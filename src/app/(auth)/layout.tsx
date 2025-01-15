fb0f2ee6de3f545214a55691a4f2c111c784f833
Boren Xue <53597966+borenx1@users.noreply.github.com>
2025-01-15 21:45:35 +1300

    Add navbar to login page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,38 @@
+import Link from 'next/link';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main className="h-full">
-      <div className="p-4 sm:p-8">{children}</div>
-    </main>
+    <>
+      <header className="flex items-center px-4 py-2 shadow-sm sm:px-16">
+        <div className="text-lg sm:text-xl">
+          <Link href="/">Investment Manager</Link>
+        </div>
+        <NavigationMenu className="ml-auto">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/login" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Login
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </header>
+      <main>
+        <div className="p-4 sm:p-8">{children}</div>
+      </main>
+    </>
   );
 }
