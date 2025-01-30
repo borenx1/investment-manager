d1bfa0620a2c05cdc2326ab90d3147134aade4f6
Boren Xue <53597966+borenx1@users.noreply.github.com>
2025-01-31 12:49:20 +1300

    Add filters to Capital Changes table

--- src/app/(authenticated)/capital/CapitalDataTable.tsx
+++ src/app/(authenticated)/capital/CapitalDataTable.tsx
@@ -1,14 +1,24 @@
 'use client';
 
-import { use } from 'react';
+import { use, useEffect, useMemo, useState } from 'react';
 import { format } from 'date-fns';
 import {
   ColumnDef,
   getCoreRowModel,
+  getFilteredRowModel,
   getPaginationRowModel,
   useReactTable,
 } from '@tanstack/react-table';
-import { EllipsisVertical, Pencil, Trash2 } from 'lucide-react';
+import {
+  ArrowUpDown,
+  CircleDollarSign,
+  EllipsisVertical,
+  Minus,
+  Pencil,
+  Plus,
+  Trash2,
+  X,
+} from 'lucide-react';
 
 import { removeCapitalTransaction } from '@/lib/actions';
 import { convertUTCDate, formatDecimalPlaces } from '@/lib/utils';
@@ -23,6 +33,7 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
+import { useResourceStore } from '@/providers/resource-store-provider';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { DataTable, DataTablePagination } from '@/components/ui/data-table';
@@ -37,6 +48,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 import AddEditCapitalTransactionDialog, {
   type Transaction,
 } from '@/components/AddEditCapitalTransactionDIalog';
+import SelectFilter from '@/components/SelectFilter';
 
 export const columns: ColumnDef<Transaction>[] = [
   {
@@ -55,10 +67,28 @@ export const columns: ColumnDef<Transaction>[] = [
     accessorKey: 'portfolioAccount.name',
     header: 'Account',
     minSize: 80,
+    filterFn: (row, _, filterValue) => {
+      if (typeof filterValue === 'number') {
+        return row.original.portfolioAccount.id === filterValue;
+      } else if (typeof filterValue === 'string') {
+        return String(row.original.portfolioAccount.id) === filterValue;
+      }
+      return true;
+    },
   },
   {
     accessorKey: 'asset.ticker',
     header: 'Asset',
+    filterFn: (row, _, filterValue) => {
+      if (Array.isArray(filterValue)) {
+        return filterValue.some((value) => row.original.asset.id === value?.id);
+      } else if (typeof filterValue === 'number') {
+        return row.original.asset.id === filterValue;
+      } else if (typeof filterValue === 'string') {
+        return String(row.original.asset.id) === filterValue;
+      }
+      return true;
+    },
   },
   {
     accessorKey: 'assetEntry.amount',
@@ -74,7 +104,8 @@ export const columns: ColumnDef<Transaction>[] = [
     minSize: 80,
   },
   {
-    accessorKey: 'feeIncomeEntry.amount',
+    id: 'feeIncomeEntry_amount',
+    accessorFn: (row) => row.feeIncomeEntry?.amount,
     header: () => <div className="text-right">Fee</div>,
     cell: ({ row }) => {
       if (!row.original.feeIncomeEntry) {
@@ -105,6 +136,20 @@ export const columns: ColumnDef<Transaction>[] = [
           )}
         </div>
       );
+    },
+    filterFn: (row, _, filterValue) => {
+      if (Array.isArray(filterValue)) {
+        return filterValue.some((value) => {
+          const amount = parseFloat(row.original.assetEntry.amount);
+          if (value.value === 'contributions') {
+            return amount >= 0;
+          } else if (value.value === 'drawings') {
+            return amount < 0;
+          }
+          return true;
+        });
+      }
+      return true;
     },
   },
   {
@@ -186,16 +231,82 @@ export default function CapitalDataTable({
   data: Promise<Transaction[]>;
 }) {
   const data = use(dataPromise);
+  const activeAccount = useResourceStore((state) => state.activeAccount);
+  const assets = useResourceStore((state) => state.assets);
+  const assetOptions = useMemo(
+    () =>
+      assets.map((asset) => ({
+        ...asset,
+        value: String(asset.id),
+        label: asset.ticker,
+      })),
+    [assets],
+  );
+  const [assetFilter, setAssetFilter] = useState<typeof assetOptions>([]);
+  const typeOptions = useMemo(
+    () =>
+      [
+        { value: 'contributions', label: 'Contributions', icon: Plus },
+        { value: 'drawings', label: 'Drawings', icon: Minus },
+      ] as const,
+    [],
+  );
+  const [typeFilter, setTypeFilter] = useState<(typeof typeOptions)[number][]>(
+    [],
+  );
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
     initialState: { pagination: { pageSize: 20 } },
   });
 
+  useEffect(() => {
+    table.getColumn('portfolioAccount_name')?.setFilterValue(activeAccount?.id);
+  }, [table, activeAccount]);
+  useEffect(() => {
+    table
+      .getColumn('asset_ticker')
+      ?.setFilterValue(assetFilter.length ? assetFilter : undefined);
+  }, [table, assetFilter]);
+  useEffect(() => {
+    table
+      .getColumn('type')
+      ?.setFilterValue(typeFilter.length ? typeFilter : undefined);
+  }, [table, typeFilter]);
+
   return (
     <div className="space-y-4">
+      <div className="flex items-center space-x-2">
+        <SelectFilter
+          name="Asset"
+          icon={CircleDollarSign}
+          options={assetOptions}
+          values={assetFilter}
+          onChange={setAssetFilter}
+        />
+        <SelectFilter
+          name="Type"
+          icon={ArrowUpDown}
+          options={typeOptions}
+          values={typeFilter}
+          onChange={setTypeFilter}
+        />
+        {!!(assetFilter.length || typeFilter.length) && (
+          <Button
+            variant="outline"
+            onClick={() => {
+              setAssetFilter([]);
+              setTypeFilter([]);
+            }}
+          >
+            Reset
+            <X />
+          </Button>
+        )}
+      </div>
       <div className="rounded-lg border">
         <DataTable table={table} gridLines />
       </div>
---
+++ src/components/SelectFilter.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import { useCallback } from 'react';
+import type { LucideIcon } from 'lucide-react';
+
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Separator } from '@/components/ui/separator';
+
+export default function SelectFilter<
+  T extends { value: string; label: string; icon?: LucideIcon },
+>({
+  name,
+  icon: Icon,
+  options,
+  values,
+  onChange,
+}: {
+  name: string;
+  icon?: LucideIcon;
+  options?: Readonly<T[]>;
+  values?: Readonly<T[]>;
+  onChange?: (values: T[]) => void;
+}) {
+  const toggleValue = useCallback(
+    (value: T) => {
+      if (!values?.length) {
+        return [value];
+      }
+      if (values.some((v) => v.value === value.value)) {
+        return values.filter((v) => v.value !== value.value);
+      }
+      if (!options?.some((opt) => opt.value === value.value)) {
+        return [...values];
+      }
+      // Return the values in the same order as options.
+      const newValues = [...values, value];
+      return newValues.sort((a, b) => {
+        const indexA = options.findIndex((v) => v.value === a.value);
+        const indexB = options.findIndex((v) => v.value === b.value);
+        return indexA - indexB;
+      });
+    },
+    [values, options],
+  );
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="border-dashed">
+          {Icon && <Icon />}
+          {name}
+          {!!values?.length && (
+            <>
+              <Separator orientation="vertical" />
+              {values.length < 3 ? (
+                values.map((value) => (
+                  <Badge
+                    key={value.value}
+                    variant="secondary"
+                    className="font-normal"
+                  >
+                    {value.label}
+                  </Badge>
+                ))
+              ) : (
+                <Badge variant="secondary" className="font-normal">
+                  {values.length} selected
+                </Badge>
+              )}
+            </>
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      {(!!options?.length || !!values?.length) && (
+        <DropdownMenuContent align="start">
+          {options?.map((opt) => (
+            <DropdownMenuItem
+              key={opt.value}
+              onClick={(e) => {
+                e.preventDefault();
+                if (onChange) {
+                  onChange(toggleValue(opt));
+                }
+              }}
+            >
+              <Checkbox
+                checked={
+                  values?.some((value) => value.value === opt.value) ?? false
+                }
+              />
+              {opt.icon && <opt.icon className="text-muted-foreground" />}
+              {opt.label}
+            </DropdownMenuItem>
+          ))}
+          {!!values?.length && (
+            <>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="justify-center"
+                onClick={() => onChange?.([])}
+              >
+                Clear filters
+              </DropdownMenuItem>
+            </>
+          )}
+        </DropdownMenuContent>
+      )}
+    </DropdownMenu>
+  );
+}
