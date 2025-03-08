a26bb602b98a7f9512a6e70c9b926b7c1e4c04d5
Boren Xue <53597966+borenx1@users.noreply.github.com>
2025-03-08 23:58:43 +1300

    Apply Geist font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} h-full font-sans antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
